9c5d9b3f38346096e47db458d9170f0b2a116fa4
Cristine Ihara <cristine.ihara@gmail.com>
2019-10-03 15:33:40 -0400

    changes from pds

--- LithoUXComponents/Classes/Models/FlowControllers/THUXAppOpenFlowController.swift
+++ LithoUXComponents/Classes/Models/FlowControllers/THUXAppOpenFlowController.swift
@@ -7,8 +7,11 @@
 
 import UIKit
 
-open class THUXAppOpenFlowController: NSObject {
+open class THUXAppOpenFlowController: THUXFlowCoordinator {
     open var splashViewModel: THUXSplashViewModel?
     open var loginViewModel: THUXLoginViewModel?
     open var registrationViewModel: THUXRegistrationViewModel?
+    
+    public init() {}
+    open func initialVC() -> UIViewController? { return nil }
 }
